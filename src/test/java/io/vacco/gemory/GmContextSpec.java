9f50e5dcb42ae93d187d73a72197cd413a9f5e3e
Jesus Zazueta <jjzazuet@vacco.io>
2021-04-30 20:41:54 -0400

    - Adding tab view test. - Additional CI checks.

--- src/test/java/io/vacco/gemory/GmContextSpec.java
+++ src/test/java/io/vacco/gemory/GmContextSpec.java
@@ -10,6 +10,7 @@ import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import org.junit.runner.RunWith;
 
+import java.awt.*;
 import java.time.LocalDate;
 import java.util.Objects;
 
@@ -21,44 +22,62 @@ public class GmContextSpec {
 
   public static class ViewFoo extends StackPane {
     public ViewFoo() {
-      Node root = root(r -> r.scrollPane((sp, c) -> {
-        sp.setFitToWidth(true);
-        c.vBox((v, $) -> {
-          $.label(lb -> lb.setText("Hello World"));
-          $.textField(tf -> tf.setText("How do we turn this into a controlled component?"));
-          $.button(bt -> bt.setText("Submit"));
-          $.menuButton((mb, mbc) -> {
-            mb.setText("Menu button");
-            mbc.item(mi -> mi.setText("Menu button item"));
+      Node lol = root(r -> r.tabPane((tp, tpc) -> {
+        tpc.tab((tab, tc) -> {
+          tab.setText("Tab 0");
+          tc.vBox((vb, $) -> {
+            $.textField(tf -> tf.setText("How do we turn this into a controlled component?"));
+            $.button(bt -> bt.setText("Submit"));
+            $.menuButton((mb, mbc) -> {
+              mb.setText("Menu button");
+              mbc.item(mi -> mi.setText("Menu button item"));
+            });
+            $.splitMenuButton(mb -> {
+              mb.mb.setText("Split Menu button");
+              mb.item(mi -> mi.setText("Split menu item"));
+            });
+            $.label(lb -> lb.setText("A label"));
           });
-          $.splitMenuButton(mb -> {
-            mb.mb.setText("Split Menu button");
-            mb.item(mi -> mi.setText("Split menu item"));
+        });
+
+        tpc.tab((tab, tc) -> {
+          tab.setText("Tab 1");
+          tc.vBox((vb, $) -> {
+            $.label(lb -> lb.setText("Hello World"));
+            $.separator(s -> s.orientationProperty().setValue(Orientation.HORIZONTAL));
+            $.checkBox(cb -> cb.setText("Check box"));
+            $.choiceBox(cb -> cb.setValue("Choice box"));
+            $.comboBox(cb -> cb.setPromptText("Combo box"));
+            $.textArea(ta -> ta.setPromptText("Text area..."));
+            $.toggleButton(tb -> tb.setText("Toggle button"));
+            $.slider(sl -> sl.valueProperty().setValue(50));
+            $.spinner(sn -> sn.setPromptText("Spinner"));
+            $.datePicker(dp -> dp.valueProperty().setValue(LocalDate.now()));
+            $.hyperLink(hl -> hl.setText("https://vacco.io"));
           });
-          $.label(lb -> lb.setText("A label"));
-          $.separator(s -> s.orientationProperty().setValue(Orientation.HORIZONTAL));
-          $.checkBox(cb -> cb.setText("Check box"));
-          $.choiceBox(cb -> cb.setValue("Choice box"));
-          $.comboBox(cb -> cb.setPromptText("Combo box"));
-          $.textArea(ta -> ta.setPromptText("Text area..."));
-          $.toggleButton(tb -> tb.setText("Toggle button"));
-          $.slider(sl -> sl.valueProperty().setValue(50));
-          $.spinner(sn -> sn.setPromptText("Spinner"));
-          $.datePicker(dp -> dp.valueProperty().setValue(LocalDate.now()));
-          $.hyperLink(hl -> hl.setText("https://vacco.io"));
-          $.passwordField(pf -> pf.setPromptText("Password input"));
-          $.progressBar(pb -> pb.progressProperty().setValue(50));
-          $.progressIndicator(pi -> pi.progressProperty().setValue(0));
-          $.radioButton(rb -> rb.setText("Radio button"));
-          $.scrollBar(sb -> sb.setValue(50));
-          $.colorPicker(cp -> cp.setValue(Color.ALICEBLUE));
-          $.imageView(Objects.requireNonNull(GmContextSpec.class.getResource("/gir-dog.png")), iv -> {
-            iv.maxWidth(266);
-            iv.maxHeight(333);
+        });
+
+        tpc.tab((tab, tc) -> {
+          tab.setText("Tab 2");
+          tc.scrollPane((sp, spc) -> {
+            sp.setFitToWidth(true);
+            spc.vBox((v, $) -> {
+              $.passwordField(pf -> pf.setPromptText("Password input"));
+              $.progressBar(pb -> pb.progressProperty().setValue(50));
+              $.progressIndicator(pi -> pi.progressProperty().setValue(0));
+              $.radioButton(rb -> rb.setText("Radio button"));
+              $.scrollBar(sb -> sb.setValue(50));
+              $.colorPicker(cp -> cp.setValue(Color.ALICEBLUE));
+              $.imageView(Objects.requireNonNull(GmContextSpec.class.getResource("/gir-dog.png")), iv -> {
+                iv.maxWidth(266);
+                iv.maxHeight(333);
+              });
+            });
           });
         });
       }));
-      getChildren().add(root);
+
+      getChildren().add(lol);
     }
   }
 
@@ -73,9 +92,13 @@ public class GmContextSpec {
 
   static {
     it("Can open a JavaFx shell", () -> {
-      System.setProperty("prism.text", "t2k");
-      System.setProperty("prism.lcdtext", "false");
-      Application.launch(SpecShell.class);
+      if (GraphicsEnvironment.isHeadless()) {
+        System.out.println("Skipping UI test, most likely inside CI environment.");
+      } else {
+        System.setProperty("prism.text", "t2k");
+        System.setProperty("prism.lcdtext", "false");
+        Application.launch(SpecShell.class);
+      }
     });
   }
 
