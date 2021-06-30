7c069cf8a5b2f7b5b9797e40ba46090af68caa7a
Jesus Zazueta <jjzazuet@vacco.io>
2021-06-29 22:34:18 -0400

    - Switching to OSS publishing.

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ version = "0.1.0"
 configure<io.vacco.oss.gitflow.GsPluginProfileExtension> {
   addJ8Spec()
   addClasspathHell()
-  sharedLibrary(true, true)
+  sharedLibrary(true, false)
 }
 
 dependencies {
