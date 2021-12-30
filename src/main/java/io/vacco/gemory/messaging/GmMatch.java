63b1ef791e3ef5861ee75f9685913ca062a90a3e
jjzazuet <jjzazuet@vacco.io>
2021-12-30 02:01:07 -0500

    - Restoring default pattern matcher action handler.

--- src/main/java/io/vacco/gemory/messaging/GmMatch.java
+++ src/main/java/io/vacco/gemory/messaging/GmMatch.java
@@ -5,18 +5,29 @@ import java.util.function.*;
 
 public class GmMatch<S> {
 
-  private final Map<Class<?>, Function<?, S>> matchFn = new HashMap<>();
+  private final Map<Class<?>, Function<?, S>> matchFnIdx = new HashMap<>();
+  private Supplier<S> defaultFn;
 
   public <A extends GmAction<?>> GmMatch<S> on(Class<? extends A> pattern, Function<A, S> cn) {
-    matchFn.put(pattern, cn);
+    matchFnIdx.put(pattern, cn);
+    return this;
+  }
+
+  public GmMatch<S> orElse(Supplier<S> defaultFn) {
+    this.defaultFn = defaultFn;
     return this;
   }
 
   @SuppressWarnings("unchecked")
   public <A extends GmAction<?>> S apply(A action) {
-    Function<A, S> fn = (Function<A, S>) matchFn.get(action.getClass());
+    Function<A, S> fn = (Function<A, S>) matchFnIdx.get(action.getClass());
     if (fn != null) { return fn.apply(action); }
-    return null;
+    else if (defaultFn != null) { return defaultFn.get(); }
+    String msg = String.format(
+        "No action handler defined for action [%s], and no fallback handler provided. Registered handlers: %s",
+        action.getClass(), matchFnIdx
+    );
+    throw new IllegalStateException(msg);
   }
 
 }
